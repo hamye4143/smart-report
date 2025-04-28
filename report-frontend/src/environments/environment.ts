d94342f918f08deb01bd18adeedf5f93380ee623
HYE <hamye4143@gmail.com>
2025-04-28 13:48:31 +0900

    CORS ERROR 수정

--- report-frontend/src/environments/environment.ts
+++ report-frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://smart-report-backend.onrender.com' // localhost:5000
+  apiUrl:'https://smart-report-backend.onrender.com'
   // apiUrl: 'http://localhost:5000', // 'https://smart-report-backend.onrender.com' // localhost:5000
 };
 
